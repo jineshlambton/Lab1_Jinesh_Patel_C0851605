2a351e819c5a2f491e4bef865e8374df5cea1f3a
Jinesh Patel <c0851605@mylambton.ca>
2022-06-07 18:07:37 -0600

    Google map setup is completed

--- app/src/main/java/com/example/lab1_jinesh_patel_c0851605/Home.java
+++ app/src/main/java/com/example/lab1_jinesh_patel_c0851605/Home.java
@@ -1,14 +1,56 @@
 package com.example.lab1_jinesh_patel_c0851605;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Home extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Home extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
+
+    List<Marker> arrMarkers = new ArrayList<>();
+    List<LatLng> arrLatLong = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        loadMap();
+    }
+
+    void loadMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.fGoogleMap);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+        map = googleMap;
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                MarkerOptions markerOptions = new MarkerOptions().position(latLng);
+                Marker marker = map.addMarker(markerOptions);
+
+                arrLatLong.add(latLng);
+                arrMarkers.add(marker);
+            }
+        });
     }
 }
